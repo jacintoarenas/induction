60d8e9a1a736ee776b1a367278ad8b014c25082c
Jacinto Arenas and Miguel Rodriguez <jacinto.arenasvelasco+miguel.rodriguez@santander.co.uk>
2020-03-12 16:37:12 +0000

    feign exercise

--- induction.manager/src/main/java/uk/co/santander/induction/manager/Application.java
+++ induction.manager/src/main/java/uk/co/santander/induction/manager/Application.java
@@ -2,7 +2,11 @@ package uk.co.santander.induction.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+//https://cloud.spring.io/spring-cloud-netflix/multi/multi_spring-cloud-feign.htmlq
+//@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
